fc01f7c5146401c0bed68649311342b4f29655f3
Mahmoud Dahy <dahym2028@gmail.com>
2025-11-29 17:45:33 +0200

    Add new portfolio images and update asset references

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  Assets._();
+  static const String booklyProtfolio = 'assets/bookly_protfolio.png';
+  static const String weatherProtfolio = 'assets/weather_protfolio.jpg';
+  static const String suitsProtfolio = 'assets/suits_profolio.png';
+  static const String fruitsProtfolio = 'assets/fruits_protfolio.png';
+  static const String booklyProtfolio2 = 'assets/bookly_protfolio_2.png';
+  static const String profileProtfolio = 'assets/profile_protfolio.jpg';
+}
+
+/**
+ * please add booklyProtfolio image in project section in Bookly App
+ * please add weatherProtfolio image in project section in Weather App
+ * please add suitsProtfolio image in project section in Suits App
+ * please add fruitsProtfolio image in project section in Fruits App
+ */
